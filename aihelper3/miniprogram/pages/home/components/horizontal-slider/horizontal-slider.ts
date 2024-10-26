397b4d71853e465f8f04fe17ed390c0c4d230908
xiaoRuMiLi <2072836794@qq.com>
2024-10-26 22:55:53 +0800

    n

--- aihelper3/miniprogram/pages/home/components/horizontal-slider/horizontal-slider.ts
+++ aihelper3/miniprogram/pages/home/components/horizontal-slider/horizontal-slider.ts
@@ -5,6 +5,15 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    itemList: {
+      type: Array,
+      value: [
+        {title: "测试标题", text: "测试使用文本内容", img: "/static/image/person/smallgirl-removebg-preview.png"},
+        {title: "测试标题", text: "测试使用文本内容", img: "/static/image/person/smallgirl-removebg-preview.png"},
+        {title: "测试标题", text: "测试使用文本内容", img: "/static/image/person/smallgirl-removebg-preview.png"},
+        {title: "测试标题", text: "测试使用文本内容", img: "/static/image/person/smallgirl-removebg-preview.png"}
+      ]
+    }
 
   },
 
@@ -12,6 +21,7 @@ Component({
    * 组件的初始数据
    */
   data: {
+    
 
   },
 
@@ -19,6 +29,13 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    handleTapItem(e: any)
+    {
+      // 抛出元素被点击事件
+      this.triggerEvent("bindTapItem", {index: e.currentTarget.dataset.key});
+      
+    },
+    
 
   }
 })
